63252a79a62715840de2a5405350ef19d1417e0b
ybvanjaneya <ybvanjaneyachoda464@gmail.com>
2023-09-10 12:30:31 +0530

    changed roll

---
+++ asgn4_21CS10020_21CS10060.c
@@ -0,0 +1,10 @@
+// Simma Pavan Kumar     21CS10060
+// Choda Y B V Anjaneya  21CS10020
+#include <stdio.h>
+extern int yyparse();
+
+int main() {
+    printf("-------------------- Parsing Line No. 1  --------------------\n\n");
+    yyparse();
+    return 0;
+}
