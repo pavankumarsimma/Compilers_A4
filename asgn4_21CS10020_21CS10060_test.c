291cc7a7a2a54aafcf5e0478b92c0803faaec2fc
pavankumarsimma <pavankumarsimma08@gmail.com>
2023-09-11 10:26:04 +0530

    nothing

--- asgn4_21CS10020_21CS10060_test.c
+++ asgn4_21CS10020_21CS10060_test.c
@@ -2,6 +2,8 @@
 //  Simma Pavan Kumar -> 21CS10060
 //  Choda Y B V Anjaneya -> 21CS10020
 
+const char *solve(int b[const static 8], int c[static 9], int d[], int e[const *]);
+
 void func(int * f1, const int f2, volatile int f3, ...) {
     auto int a;
     register int b;
@@ -9,19 +11,12 @@ void func(int * f1, const int f2, volatile int f3, ...) {
     static int d = 4;
 }
 
-const char *solve(int b[const static 8], int c[static 9], int d[], int e[const *]);
 
 enum E1;
 enum E2 { T1, T2 };
 enum { T3 = 0, T4 } E3;
 
 signed main() {
-    int a[6] = { 1, 2, [2]=3 };
-    char b[] = "This is for testing";
-    char c1 = 's';
-    char *c2 = &c1;
-    *c2 = 'c';
-    char **d = (char) { "j", b, "pqr" };
 
     unsigned long n1 = +1239;
     short n2 = ~16;
@@ -30,6 +25,16 @@ signed main() {
     _Bool n5 = !1;
     double _Complex n6;
     double _Imaginary n7;
+
+
+    int a[6] = { 1, 2, [2]=3 };
+    char b[] = "This is for testing";
+    char c1 = 's';
+    char *c2 = &c1;
+    *c2 = 'c';
+    char **d = (char) { "j", b, "pqr" };
+
+    
 
     n1 *= n1, n1 /= n1, n1 %= n1;
     n1 += (n1 -= n1);
@@ -65,6 +70,15 @@ signed main() {
     while(n2--)
         goto   SOME_THING;
 
+
+    int t1, t2, t3, t4, t5, t6;
+
+    t1 = t1 + t2 - t3 / t4 * t5 % t6;
+    t2 = t2<<2;
+    t2 = t2>>2;
+    t1 = t2 & t3 | t4 ^ t5;
+
+
     for(;;)
     for(int i;;)
     for(int i=0, j=1; i!=j; i++) {
@@ -76,13 +90,7 @@ signed main() {
         n2++;
     } while(n2 < 0);
 
-    int t1, t2, t3, t4, t5, t6;
-
-    t1 = t1 + t2 - t3 / t4 * t5 % t6;
-    t2 = t2<<2;
-    t2 = t2>>2;
-    t1 = t2 & t3 | t4 ^ t5;
-
+    
     func(1,2,3,4,5,6);
 
     pavan.a = 3;
